5cba708a8cee7303b46ff1647394766813522602
Марина <marina.morozevich@gmail.com>
2023-08-08 12:20:32 +0300

    добавлено описание

--- src/main/java/ru/morozevich/triplecalculator/service/ResultWriterService.java
+++ src/main/java/ru/morozevich/triplecalculator/service/ResultWriterService.java
@@ -2,6 +2,6 @@ package ru.morozevich.triplecalculator.service;
 
 public class ResultWriterService {
     public static void printResult(int a, int b, int c, String d) {
-        System.out.println(a + " " + b + " " + c + " " + d);
+        System.out.println("Число 1:"+a + " Число 2:" + b + " Результат:" + c + " Выполненное действие:" + d);
     }
 }
