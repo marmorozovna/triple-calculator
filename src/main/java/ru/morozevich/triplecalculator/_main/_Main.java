348f35bfc75b945ac0eaf07343a5b27d0ea24042
Марина <marina.morozevich@gmail.com>
2023-08-08 12:16:18 +0300

    переименование и ввод с клавиатуры

--- src/main/java/ru/morozevich/triplecalculator/_main/_Main.java
+++ src/main/java/ru/morozevich/triplecalculator/_main/_Main.java
@@ -1,12 +1,16 @@
 package ru.morozevich.triplecalculator._main;
 
+import java.util.Scanner;
+
 import static ru.morozevich.triplecalculator.service.ResultWriterService.printResult;
 import static ru.morozevich.triplecalculator.util.Calculator.*;
 
 public class _Main {
     public static void main(String[] args) {
-        int x = 7;
-        int y = 8;
+        Scanner sc;
+        sc = new Scanner (System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
         int result = sum(x, y);
         printResult(x, y, result, "сложение");
         result = deduct(x, y);
