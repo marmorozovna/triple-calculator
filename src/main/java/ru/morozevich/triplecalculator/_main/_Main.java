5265299a4641634330c886b901987645c4c9deea
marmorozovna <marina.morozevich@gmail.com>
2023-08-13 14:30:12 +0300

    убрала статический импорт и совместила sc

--- src/main/java/ru/morozevich/triplecalculator/_main/_Main.java
+++ src/main/java/ru/morozevich/triplecalculator/_main/_Main.java
@@ -1,18 +1,17 @@
 package ru.morozevich.triplecalculator._main;
 
-import java.util.Scanner;
+import ru.morozevich.triplecalculator.service.ResultWriterService;
+import ru.morozevich.triplecalculator.util.Calculator;
 
-import static ru.morozevich.triplecalculator.service.ResultWriterService.printResult;
-import static ru.morozevich.triplecalculator.util.Calculator.*;
+import java.util.Scanner;
 
 public class _Main {
     public static void main(String[] args) {
-        Scanner sc;
-        sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int num1 = sc.nextInt();
         int num2 = sc.nextInt();
-        printResult(num1, num2, sum(num1, num2), "сложение");
-        printResult(num1, num2, deduct(num1, num2), "вычитание");
-        printResult(num1, num2, multiply(num1, num2), "умножение");
+        ResultWriterService.printResult(num1, num2, Calculator.sum(num1, num2), "сложение");
+        ResultWriterService.printResult(num1, num2, Calculator.deduct(num1, num2), "вычитание");
+        ResultWriterService.printResult(num1, num2, Calculator.multiply(num1, num2), "умножение");
     }
 }
